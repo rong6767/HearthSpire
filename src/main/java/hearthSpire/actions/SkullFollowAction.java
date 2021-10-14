5104425e1a2a3ca41e0744d8bad0c21b5954eb73
wenx29 <wenx29@mcmaster.ca>
2021-10-13 22:21:15 -0400

    initial commit

---
+++ src/main/java/hearthSpire/actions/SkullFollowAction.java
@@ -0,0 +1,27 @@
+package hearthSpire.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import java.util.Iterator;
+
+public class SkullFollowAction extends AbstractGameAction {
+    public SkullFollowAction() {
+        this.duration = 0.001F;
+    }
+
+    public void update() {
+            Iterator var1 = DrawCardAction.drawnCards.iterator();
+
+            while(var1.hasNext()) {
+                AbstractCard c = (AbstractCard)var1.next();
+                if (c.costForTurn != 0 && !c.freeToPlayOnce) {
+                    c.costForTurn = 0;
+                }
+            }
+        isDone = true;
+    }
+}
---
+++ src/main/java/hearthSpire/cards/PuzzleBoxofYoggSaronRareSkill.java
@@ -0,0 +1,75 @@
+package hearthSpire.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PlatedArmorPower;
+import hearthSpire.DefaultMod;
+import hearthSpire.actions.SkullFollowAction;
+import hearthSpire.characters.TheDefault;
+
+import static hearthSpire.DefaultMod.makeCardPath;
+
+public class PuzzleBoxofYoggSaronRareSkill extends AbstractDynamicCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * A Better Defend Gain 1 Plated Armor. Affected by Dexterity.
+     */
+
+    // TEXT DECLARATION
+
+    public static final String ID = DefaultMod.makeID(SkullofIroncladRareSkill.class.getSimpleName());
+    public static final String IMG = makeCardPath("Skill.png");
+
+    // /TEXT DECLARATION/
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = CardColor.COLORLESS;
+
+    private static final int COST = 3;
+    private static final int UPGRADE_REDUCED_COST = 3;
+
+    private static final int DRAW = 2;
+
+    private static final int BLOCK = 1;
+    private static final int UPGRADE_PLUS_BLOCK = 2;
+
+    // /STAT DECLARATION/
+
+
+    public PuzzleBoxofYoggSaronRareSkill() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = DRAW;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if(p.hand.getBottomCard().cardID.equals(ID) || p.hand.getTopCard().cardID.equals(ID)) {
+            AbstractDungeon.actionManager.addToBottom(
+                    new DrawCardAction(DRAW, new SkullFollowAction()));
+        }
+        else{
+            AbstractDungeon.actionManager.addToBottom(
+                    new DrawCardAction(p,DRAW));
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+            initializeDescription();
+        }
+    }
+}
