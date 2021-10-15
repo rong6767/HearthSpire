030bef1e3723c842979e74ad36c7e58949e6043c
wenx29 <wenx29@mcmaster.ca>
2021-10-14 20:24:59 -0400

    Add EviscerateCommonAttack.java

---
+++ src/main/java/hearthSpire/cards/EviscerateCommonAttack.java
@@ -0,0 +1,63 @@
+package hearthSpire.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hearthSpire.DefaultMod;
+
+import static hearthSpire.DefaultMod.makeCardPath;
+
+public class EviscerateCommonAttack extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(EviscerateCommonAttack.class.getSimpleName());
+    public static final String IMG = makeCardPath("eviscerate.png");
+
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = CardColor.GREEN;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 5;
+    private static final int UPGRADE_PLUS_DMG = 2;
+
+    // /STAT DECLARATION/
+
+
+    public EviscerateCommonAttack() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        magicNumber = baseMagicNumber = 12;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int count = AbstractDungeon.actionManager.cardsPlayedThisTurn.size();
+        if(count == 1){
+            this.addToBot( new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn),
+                    AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        }
+        else{
+            this.addToBot(
+                    new DamageAction(m, new DamageInfo(p, magicNumber, damageTypeForTurn),
+                            AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        }
+
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeMagicNumber(6);
+            initializeDescription();
+        }
+    }
+}
